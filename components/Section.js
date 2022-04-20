5c1e7ef2be658de86b5e9eabeb7a734c6a3b7e13
Ehm27 <erick@coolhumans.team>
2022-04-20 12:14:05 -0500

    Card component to put movies in each section

--- components/Section.js
+++ components/Section.js
@@ -1,8 +1,16 @@
+import Card from "./Card.js";
+
 const Section = ({ genre, videos }) => {
-  console.log(videos);
   return (
-    <div>
+    <div className="section">
       <h3>{genre}</h3>
+      <div className="video-feed">
+        {videos.map((video) => (
+          <a key={video.id} href={`/video/${video.slug}`}>
+            <Card thumbnail={video.thumbnail} />
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
